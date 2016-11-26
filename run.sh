1a8cbebbeba6aaf925cd5b95dcc0166995e26852
Joakim <bannan@gmail.com>
2016-11-26 12:53:39 +0100

    Removed unneccesary commands

--- run.sh
+++ run.sh
@@ -14,11 +14,5 @@ echo -e "please make sure to edit the config.json file followed by [ENTER]" && r
 
 ansible-playbook setup.yml -i HOSTS --ask-sudo-pass  --module-path ./ansible_modules --extra-vars "@config.json"
 
-dropbox start -i > /dev/null 2>&1 &
-/opt/extras.ubuntu.com/variety/bin/variety > /dev/null 2>&1 &
-plank > /dev/null 2>&1 &
-steam > /dev/null 2>&1 &
-emacs > /dev/null 2>&1 &
-
 source ~/.bashrc
 exit 0
